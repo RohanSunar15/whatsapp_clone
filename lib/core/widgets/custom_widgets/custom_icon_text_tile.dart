dfc9752227670163b8bcb3162ed3ee136af2d62c
Rohan Sunar <rohanksunar15@gmail.com>
2025-06-02 12:27:38 +0530

    Changed in Custom Icon Text tile added parameter of icon color

--- lib/core/widgets/custom_widgets/custom_icon_text_tile.dart
+++ lib/core/widgets/custom_widgets/custom_icon_text_tile.dart
@@ -4,19 +4,24 @@ import 'package:whatsapp_clone/core/theme/app_color.dart';
 class IconTextTile extends StatelessWidget {
   final IconData icon;
   final String title;
-  final String value;
+  final String? value;
   final Color? valueColor;
   final FontWeight? valueFontWeight;
   final VoidCallback? onTap;
+  final Color? iconColor;
+
+
 
   const IconTextTile({
     super.key,
     required this.icon,
     required this.title,
-    required this.value,
+    this.value,
     this.valueColor,
     this.valueFontWeight,
     this.onTap,
+    this.iconColor,
+
   });
 
   @override
@@ -28,7 +33,7 @@ class IconTextTile extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Icon(icon, size: 24, color: Colors.grey[700]),
+            Icon(icon, size: 24, color: iconColor ?? Colors.grey[700],),
             const SizedBox(width: 16),
             Expanded(
               child: Column(
@@ -40,8 +45,9 @@ class IconTextTile extends StatelessWidget {
                         color: AppColor.black,
                       )),
                   const SizedBox(height: 2),
+                  if(value != null)
                   Text(
-                    value,
+                    value!,
                     style: TextStyle(
                       fontSize: 12,
                       fontWeight:  valueFontWeight ?? FontWeight.normal,
