cf05c45aa00b7fcfc7c962758cee980eba95e008
Rohan Sunar <rohanksunar15@gmail.com>
2025-06-04 21:52:31 +0530

    Implemented Custom TextField in name change screen

--- lib/features/profile/view/name_change_screen.dart
+++ lib/features/profile/view/name_change_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/core/theme/app_color.dart';
 import 'package:whatsapp_clone/core/widgets/custom_widgets/custom_button.dart';
+import 'package:whatsapp_clone/core/widgets/custom_widgets/custom_textfield.dart';
 
 class NameChangeScreen extends StatefulWidget {
   const NameChangeScreen({super.key});
@@ -25,43 +26,18 @@ class _NameChangeScreenState extends State<NameChangeScreen> {
           'Name',
           style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
         ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0),
-          child: Container(
-            color: Colors.grey.shade300, // or any border color
-            height: 1.0,
-          ),
-        ),
         leadingWidth: 30,
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Column(
           children: [
-            TextFormField(
+            CustomTextField(
               controller: _nameController,
+              label: 'Your name',
               maxLength: _maxLength,
-              cursorColor: AppColor.lightGreen,
-              decoration: InputDecoration(
-                labelText: 'Your name',
-                labelStyle: TextStyle(color: AppColor.grey),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green, width: 2),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                suffixIcon: Icon(
-                  Icons.emoji_emotions_outlined,
-                  color: Colors.grey,
-                ),
-                counterText: '${_nameController.text.length}/$_maxLength',
-              ),
-              onChanged: (value) {
-                setState(() {}); // to update counter
-              },
+              suffixIcon: Icons.emoji_emotions_outlined,
+              onChanged: (value) => setState(() {}),
             ),
             SizedBox(height: 5,),
             Text(
