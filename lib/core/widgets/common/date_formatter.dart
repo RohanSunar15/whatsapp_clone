10c9d8390fa904d403c90aac7d2dd75bd4a31bbd
Rohan Sunar <rohanksunar15@gmail.com>
2025-07-19 21:18:24 +0530

    add intl package and create reusable date formatter utility using intl package

---
+++ lib/core/widgets/common/date_formatter.dart
@@ -0,0 +1,27 @@
+import 'package:intl/intl.dart';
+
+String formatChatTimestamp(DateTime timestamp) {
+  final now = DateTime.now();
+  final localTime = timestamp.toLocal();
+
+  final isSameDay =
+      now.year == localTime.year &&
+      now.month == localTime.month &&
+      now.day == localTime.day;
+
+  final yesterday = now.subtract(Duration(days: 1));
+  final isYesterday =
+      yesterday.year == localTime.year &&
+      now.month == localTime.month &&
+      now.day == localTime.day;
+
+  if (isSameDay) {
+    // e.g., 1:50 AM
+    return DateFormat('h:mm a').format(localTime);
+  } else if (isYesterday) {
+    return 'Yesterday';
+  } else {
+    // e.g., 09/02/25
+    return DateFormat('dd/MM/yy').format(localTime);
+  }
+}
